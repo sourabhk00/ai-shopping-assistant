3139adaebb0741f9187b04ecfb6eeacb64bbdf3b
sourabhk00 <156117175+sourabhk00@users.noreply.github.com>
2025-06-26 02:16:32 +0530

    Add .next/static/chunks/src_app_login_page_tsx_f94b58a9._.js 1750884392016

---
+++ .next/static/chunks/src_app_login_page_tsx_f94b58a9._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_b7367b92._.js",
+  "static/chunks/src_2c552479._.js"
+],
+    source: "dynamic"
+});
